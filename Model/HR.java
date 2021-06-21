5b40e387421d78abd52dabe30af6d79b4897212c
Kuntojishweta <86106496+Kuntojishweta@users.noreply.github.com>
2021-06-21 12:32:32 +0530

    Create HR.java

---
+++ Model/HR.java
@@ -0,0 +1,18 @@
+/**
+	 * @param depName
+	 * @param depHead
+	 */
+	public HR(String depName, String depHead) {
+		super(depName, depHead);
+	}
+	/**
+	 * @param depName
+	 */
+	public HR(String depName) {
+		super(depName);
+	}
+	public HR() {
+		super("Human Resources");
+	}
+
+}
