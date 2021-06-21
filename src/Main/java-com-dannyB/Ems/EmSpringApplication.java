63f0458b942ebfc13af5404d4f56bc9392346964
Kuntojishweta <86106496+Kuntojishweta@users.noreply.github.com>
2021-06-21 13:20:22 +0530

    Create EmSpringApplication.java

---
+++ src/Main/java-com-dannyB/Ems/EmSpringApplication.java
@@ -0,0 +1,12 @@
+package com.dannyB.EMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmSpringApplication.class, args);
+	}
+}
